ca63a131e0b7815fd7e5be5fedf452e266797bd8
DONALDBZR <andygaspard@hotmail.com>
2025-09-11 00:12:32 +0400

    [UPDATE 0.0.7] Dispatching the matched route's callback function.

--- App/Core/Router.php
+++ App/Core/Router.php
@@ -65,4 +65,29 @@ class Router {
         $method = strtoupper($method);
         $this->routes[$method][$path] = $callback;
     }
+
+    /**
+     * Dispatching the matched route's callback function.
+     * @param string $uniform_resource_identifier The URL path of the request.
+     * @param string $request_method The HTTP method of the request.
+     * @return mixed The result of the callback function, or null if the route is not found or the callback is invalid.
+     * @throws \Exception If the callback is invalid.
+     */
+    public function dispatch(string $uniform_resource_identifier, string $request_method): mixed
+    {
+        $request_method = strtoupper($request_method);
+        $path = parse_url($uniform_resource_identifier, PHP_URL_PATH) ?? "/";
+        if (!isset($this->routes[$request_method][$path])) {
+            http_response_code(404);
+            echo "404 Not Found";
+            return null;
+        }
+        $callback = $this->routes[$request_method][$path];
+        if (!is_callable($callback)) {
+            http_response_code(500);
+            echo "500 Internal Server Error";
+            return null;
+        }
+        return call_user_func($callback);
+    }
 }
