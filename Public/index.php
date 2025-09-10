9747999e1902d5750894706c7fe715953da35f50
DONALDBZR <andygaspard@hotmail.com>
2025-09-10 23:55:00 +0400

    [UPDATE 0.0.1] Initializing the project

---
+++ Public/index.php
@@ -0,0 +1,22 @@
+<?php
+require_once __DIR__ . "/../vendor/autoload.php";
+
+
+// Loading environment variables
+if (file_exists(__DIR__ . "/../.env")) {
+    $environment = parse_ini_file(__DIR__ . "/../.env");
+    foreach ($environment as $key => $value) {
+        $_ENV[$key] = $value;
+    }
+}
+require_once __DIR__ . "/../app/Core/Router.php";
+
+
+$router = new Router();
+$router->get("/", function () {
+    echo "Welcome to your PHP CMS 🚀";
+});
+$router->get("/about", function () {
+    echo "This is the About Page.";
+});
+$router->dispatch($_SERVER["REQUEST_URI"], $_SERVER["REQUEST_METHOD"]);
