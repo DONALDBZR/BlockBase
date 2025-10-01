78fd487d0c38568097faaf3d432225a0b97d0d03
DONALDBZR <andygaspard@hotmail.com>
2025-10-01 23:10:27 +0400

    [UPDATE 0.0.24] Establishing a connection to the database.

--- App/Core/Database.php
+++ App/Core/Database.php
@@ -3,6 +3,7 @@ namespace App\Core;
 
 use PDO;
 use App\Core\Logger;
+use PDOException;
 
 class Database_Handler {
     private string $host;
@@ -14,7 +15,8 @@ class Database_Handler {
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES => false
     ];
-    private PDO $connection;
+    private ?PDO $connection = null;
+    private Logger $logger;
 
     /**
      * Initializing the database handler with the given environment variables.
@@ -29,15 +31,58 @@ class Database_Handler {
         $this->schema = $_ENV["DB_SCHEMA"];
         $this->username = $_ENV["DB_USERNAME"];
         $this->password = $_ENV["DB_PASSWORD"];
+        $this->setLogger(Logger::init());
+        $this->connect();
     }
 
-    private function getConnection(): PDO
+    private function getConnection(): ?PDO
     {
         return $this->connection;
     }
 
-    private function setConnection(PDO $connection): void
+    private function setConnection(?PDO $connection): void
     {
         $this->connection = $connection;
+    }
+
+    private function getLogger(): Logger
+    {
+        return $this->logger;
+    }
+
+    private function setLogger(Logger $logger): void
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * Establishing a connection to the database.
+     * @return void
+     * @throws PDOException If an error occurs while establishing the connection.
+     */
+    private function connect(): void
+    {
+        if (!is_null($this->getConnection())) {
+            return;
+        }
+        try {
+            $data_source_name = "mysql:host={$this->host};dbname={$this->schema}";
+            $this->setConnection(
+                new PDO(
+                    $data_source_name,
+                    $this->username,
+                    $this->password,
+                    $this->options
+                )
+            );
+            $this->getLogger()->log("The database is connected.", Logger::INFO);
+        } catch (PDOException $error) {
+            $context = [
+                "file" => $error->getFile(),
+                "line" => $error->getLine(),
+                "message" => $error->getMessage()
+            ];
+            $this->getLogger()->log($error->getMessage(), Logger::ERROR, $context);
+        }
     }
 }
