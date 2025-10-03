a6f804cbceb136a5f2ef783dd4ae0f078d525e02
DONALDBZR <andygaspard@hotmail.com>
2025-10-04 01:43:45 +0400

    [UPDATE 0.0.72] Abstract class for the Object Relational Mapper.

--- App/Models/ORM/ObjectRelationalMapper.php
+++ App/Models/ORM/ObjectRelationalMapper.php
@@ -10,8 +10,8 @@ use App\Core\Database_Handler;
  * @package App\Models\ORM
  * @property-read Database_Handler $database_handler The database handler to use for queries.
  * @method array<int,self> all() Retrieving all the records from the database table.
- * @method array<int,self> find(int $id) Retrieving a single record from the database table based on the given ID.
- * @method self create(array $data) Creating a new record in the database table.
+ * @method self|null find(int $id) Retrieving a single record from the database table based on the given ID.
+ * @method int create(array $data) Creating a new record in the database table.
  * @method void update(int $id, array $data) Updating an existing record in the database table.
  * @method void delete(int $id) Deleting a record from the database table.
  */
@@ -47,23 +47,23 @@ abstract class Object_Relational_Mapper
      * @return array<int,self> The records retrieved from the database table.
      * @abstract
      */
-    abstract public function all();
+    abstract public function all(): array;
 
     /**
      * Retrieving a single record from the database table based on the given ID.
      * @param int $id The ID of the record to retrieve.
-     * @return array<int,self> The record retrieved from the database table.
+     * @return ?self The record retrieved from the database table.
      * @abstract
      */
-    abstract public function find(int $id);
+    abstract public function find(int $id): mixed;
 
     /**
      * Creating a new record in the database table.
      * @param array<string,mixed> $data The data to insert into the database table.
-     * @return self The ID of the newly created record.
+     * @return int The ID of the newly created record.
      * @abstract
      */
-    abstract public function create(array $data);
+    abstract public function create(array $data): int;
 
     /**
      * Updating an existing record in the database table.
@@ -72,7 +72,7 @@ abstract class Object_Relational_Mapper
      * @return void
      * @abstract
      */
-    abstract public function update(int $id, array $data);
+    abstract public function update(int $id, array $data): void;
 
     /**
      * Deleting a record from the database table.
@@ -80,5 +80,5 @@ abstract class Object_Relational_Mapper
      * @return void
      * @abstract
      */
-    abstract public function delete(int $id);
+    abstract public function delete(int $id): void;
 }
