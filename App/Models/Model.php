54976bec6ab6ab67d3f48bdf68740218de8c1568
DONALDBZR <andygaspard@hotmail.com>
2025-10-03 22:16:31 +0400

    [UPDATE 0.0.50] Retrieving all records from the table associated with this model.

---
+++ App/Models/Model.php
@@ -0,0 +1,98 @@
+<?php
+namespace App\Models;
+
+use App\Core\Database_Handler;
+use Attribute;
+use Enum;
+
+
+abstract class Model
+{
+    private array $attributes;
+    private Database_Handler $database_handler;
+    private string $table_name;
+
+    public function __construct(Database_Handler $database_handler, string $table_name)
+    {
+        $this->setDatabaseHandler($database_handler);
+        $this->setTableName($table_name);
+    }
+
+    private function getDatabaseHandler(): Database_Handler
+    {
+        return $this->database_handler;
+    }
+
+    private function setDatabaseHandler(Database_Handler $database_handler): void
+    {
+        $this->database_handler = $database_handler;
+    }
+
+    private function getTableName(): string
+    {
+        return $this->$table_name;
+    }
+
+    private function setTableName(string $table_name): void
+    {
+        $this->$table_name = $table_name;
+    }
+
+    /**
+     * Retrieving all records from the table associated with this model.
+     * @return array<int,Model> An array of model objects, each representing a record from the table.
+     */
+    public function get(): array
+    {
+        $query = "SELECT * FROM `{$this->getTableName()}`";
+        $response = $this->getDatabaseHandler()->get($query);
+        $models = [];
+        foreach ($response as $row) {
+            $model = new static($this->getDatabaseHandler());
+            $model->setAttributes($row);
+            $models[] = $model;
+        }
+        return $models;
+    }
+
+    public function find(mixed $identifier): ?self
+    {
+        $table_name =::getTableName();
+        $query = "SELECT * FROM `{$table_name}` WHERE identifier = :identifier";
+        $parameters = [
+            ":identifier" => $identifier
+        ];
+        $response = $this->getDatabaseHandler()->get($query, $parameters);
+        if (empty($response)) {
+            return null;
+        }
+        $model = new($this->getDatabaseHandler());
+        $model->setAttributes($response[0]);
+        return $model;
+    }
+
+    protected function setAttributes(array $data): void
+    {
+        foreach ($data as $key => $value) {
+            $this->attributes[$key] = $value;
+        }
+    }
+
+    abstract public function findAll(): array;
+
+    abstract public function create(array $data): self;
+
+    abstract public function update(string $id, array $data): self;
+
+    abstract public function delete(string $id): bool;
+
+    protected function executeQuery(string $query, array $params = []): array
+    {
+        return $this->databaseHandler->executeQuery($query, $params);
+    }
+
+    protected function executeTransaction(callable $callback): bool
+    {
+        return $this->databaseHandler->executeTransaction($callback);
+    }
+}
