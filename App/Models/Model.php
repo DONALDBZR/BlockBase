bc31426a3b96a3c9ba5a2ea3d1f698401d27978c
DONALDBZR <andygaspard@hotmail.com>
2025-10-04 20:56:27 +0400

    [UPDATE 0.0.77] Retrieving all records from the database table.

--- App/Models/Model.php
+++ App/Models/Model.php
@@ -17,24 +17,21 @@ use App\Core\Database_Handler;
  * @method array<string,mixed> getDirtyAttributes() Getting the dirty attributes.
  * @method void clearDirtyAttributes() Clearing the dirty attributes.
  */
-abstract class Model
+class Model
 {
     /**
      * The database handler for the model.
-     *
      * @var ?Database_Handler
      */
     private static ?Database_Handler $database_handler;
     /**
      * Tracking changes to the model.
-     *
      * @var array<string,mixed> $dirty_attributes The attributes that have been changed.
      */
     private array $dirty_attributes = [];
 
     /**
      * Constructor for the model.
-     *
      * @param ?Database_Handler $database_handler The database handler to use for queries.
      */
     public function __construct(
@@ -59,7 +56,8 @@ abstract class Model
      * @param array<string,mixed> $data The data to insert into the database table.
      * @return int The ID of the newly created record.
      */
-    abstract public static function post(array $data): int;
+    public static function post(array $data): int
+    {}
 
     /**
      * Updating an existing record in the database table.
@@ -67,27 +65,47 @@ abstract class Model
      * @param array<string,mixed> $data The data to update in the database table.
      * @return void
      */
-    abstract public static function put(int $id, array $data): void;
+    public static function put(int $id, array $data): void
+    {}
 
     /**
      * Retrieving a single record from the database table based on the given ID.
      * @param int $id The ID of the record to retrieve.
      * @return ?self The record retrieved from the database table, or null if not found.
      */
-    abstract public static function get(int $id): ?self;
+    public static function get(int $id): ?self
+    {}
 
     /**
      * Retrieving all records from the database table.
      * @return array<int,self> The records retrieved from the database table.
      */
-    abstract public static function all(): array;
+    public static function all(string $table_name): array
+    {
+        $database_handler = self::getDatabaseHandler();
+        $query = "SELECT * FROM {$table_name}";
+        $database_response = self::getDatabaseHandler()->get($query);
+        if (empty($database_response)) {
+            return [];
+        }
+        $response = [];
+        foreach ($database_response as $row) {
+            $record = new static(self::getDatabaseHandler());
+            foreach ($row as $attribute => $value) {
+                $record->$attribute = $value;
+            }
+            $response[] = $record;
+        }
+        return $response;
+    }
 
     /**
      * Deleting a record from the database table.
      * @param int $id The ID of the record to delete.
      * @return void
      */
-    abstract public static function delete(int $id): void;
+    public static function delete(int $id): void
+    {}
 
     /**
      * Marking an attribute as dirty.
